64413cc10ecb4863157be43d8c841af636b804ad
JaxYoun <gao88jie@qq.com>
2019-02-27 09:13:29 +0000

    补充switch后无表达式的知识

--- src/SwitchTest.go
+++ src/SwitchTest.go
@@ -1,6 +1,6 @@
 //Golang中的switch与Java中的作用相似，但是存在某些qubie
 //1.switch后支持所有的基本数据类型，ing，float，bool，string等
-//2.switch后可以不带参数或表达式，此时的case后需要写bool表达式来达到分支的目的，类似if-else语句
+//2.switch后可以不带参数或表达式，此时相当于switch true，只执行为true的case，此时的case后需要写bool表达式来达到分支的目的，类似if-else语句
 //3.case子句后可以带多个条件，表示只要满足这多个中的一个条件就可以执行这个分支
 //4.case子句中的break不是强制的【但，如果逻辑需要，可以使用break关键字达到跳出本分支，并跳出本switch的目的】，默认只会执行一个分支而不会出现条件击穿问题
 //5.跟Java一样，如果没有任何条件满足，就会执行default分支
